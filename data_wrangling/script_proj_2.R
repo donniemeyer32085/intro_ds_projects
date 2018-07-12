6c64fe2b10b0c026ecef26dc0bb634ad9677b746
donniemeyer32085 <123XYZabc#>
2018-07-11 22:13:01 -0700

    third commit

---
+++ data_wrangling/script_proj_2.R
@@ -0,0 +1,8 @@
+
+### Project: Data Wrangling Exercise 2: Dealing with missing values ###
+
+library("tidyverse")
+## Read in Data ##
+
+df <- read_csv("/home/donniemeyer32085/git/springboard_projects/data_wrangling/project_2/titanic_original.csv")
+glimpse(df)
