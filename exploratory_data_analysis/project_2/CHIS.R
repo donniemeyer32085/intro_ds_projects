c244cfdb9f1ce1a1a73d8050393a6cb8865877a8
donniemeyer32085 <123XYZabc#>
2018-07-23 16:01:21 -0700

    eda project 2

--- exploratory_data_analysis/project_2/CHIS.R
+++ exploratory_data_analysis/project_2/CHIS.R
@@ -1,3 +1,221 @@
-
-
+library(foreign)
+library(tidyverse)
+setwd("/home/donniemeyer32085/git/springboard_projects/exploratory_data_analysis/project_2/")
 ### EDA CHIS Exercise ###
+
+
+## Import Data ##
+adult <- read.dta("ADULT.dta")
+
+#variables of interest
+#RBMI: BMI Category description
+#BMI_P: BMI value
+#RACEHPR2: race
+#SRSEX: sex
+#SRAGE_P: age
+#MARIT2: Marital status
+#AB1: General Health Condition
+#ASTCUR: Current Asthma Status
+#AB51: Type I or Type II Diabetes
+#POVLL: Poverty level
+
+#select varaibles and make upper case
+adult <- select(adult, rbmi, bmi_p, racehpr2, srsex, srage_p, marit2, ab1, astcur, ab51, povll)
+dim(adult)
+names(adult) <- toupper(names(adult))
+adult
+
+
+
+## Data Exploring ##
+
+# Explore the dataset with summary and str
+str(adult)
+summary(adult)
+# Age histogram
+ggplot(adult, aes(SRAGE_P)) +
+  geom_histogram()
+# BMI value histogram
+ggplot(adult, aes(BMI_P)) +
+  geom_histogram()
+# Age colored by BMI, binwidth = 1
+ggplot(adult, aes(SRAGE_P, fill = factor(RBMI))) +
+  geom_histogram(binwidth = 1)
+#Use diff(range(adult$SRAGE_P))/30 to determine the value. Is the range divided by 30.
+diff(range(adult$SRAGE_P))/30
+
+
+
+## Cleaning the Data ##
+
+# Keep adults younger than or equal to 84
+adult <- adult[adult$SRAGE_P <= 84, ] 
+# Keep adults with BMI at least 16 and less than 52
+adult <- adult[adult$BMI_P >= 16 & adult$BMI_P < 52, ]
+# Relabel the race variable
+adult$RACEHPR2 <- factor(adult$RACEHPR2, labels = c("Latino", "Asian", "African American", "White", "PACIFIC ISLANDER", "OTHER SINGLE/MULTIPLE RACE", "INDIAN/ALASKAN NATIVE"))
+# Relabel the BMI categories variable
+adult$RBMI <- factor(adult$RBMI, labels = c("Under-weight", "Normal-weight", "Over-weight", "Obese"))
+glimpse(adult)
+table(adult$RACEHPR2)
+
+
+# The color scale used in the plot
+BMI_fill <- scale_fill_brewer("BMI Category", palette = "Reds")
+
+# Theme to fix category display in faceted plot
+fix_strips <- theme(strip.text.y = element_text(angle = 0, hjust = 0, vjust = 0.1, size = 14),
+                    strip.background = element_blank(),
+                    legend.position = "none")
+
+# Histogram, add BMI_fill and customizations
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
+  geom_histogram(binwidth = 1) +
+  fix_strips +
+  BMI_fill +
+  facet_grid(RBMI ~ .) +
+  theme_classic()
+
+# Plot 1 - Count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
+  geom_histogram(binwidth = 1) +
+  BMI_fill
+
+# Plot 2 - Density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1) +
+  BMI_fill
+
+# Plot 3 - Faceted count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
+  geom_histogram(binwidth = 1) +
+  BMI_fill +
+  facet_grid(RBMI ~ .)
+
+
+
+# Plot 4 - Faceted density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1) +
+  BMI_fill +
+  facet_grid(RBMI ~ .)
+
+
+# Plot 5 - Density histogram with position = "fill"
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1, position = "fill") +
+  BMI_fill
+
+
+
+
+# Plot 6 - The accurate histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..count../sum(..count..)), binwidth = 1, position = "fill") +
+  BMI_fill
+
+# An attempt to facet the accurate frequency histogram from before (failed)
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
+  geom_histogram(aes(y = ..count../sum(..count..)), binwidth = 1, position = "fill") +
+  BMI_fill +
+  facet_grid(RBMI ~ .)
+
+# Create DF with table()
+DF <- table(adult$RBMI, adult$SRAGE_P)
+
+# Use apply on DF to get frequency of each group: DF_freq
+DF_freq <- apply(DF, 2, function(x) x/sum(x))
+
+# Load reshape2 and use melt() on DF_freq to create DF_melted
+library(reshape2)
+DF_melted <- melt(DF_freq)
+
+
+# The initial contingency table
+DF <- as.data.frame.matrix(table(adult$SRAGE_P, adult$RBMI))
+
+# Create groupSum, xmax and xmin columns
+DF$groupSum <- rowSums(DF)
+DF$xmax <- cumsum(DF$groupSum)
+DF$xmin <- DF$xmax - DF$groupSum
+# The groupSum column needs to be removed; don't remove this line
+DF$groupSum <- NULL
+
+# Copy row names to variable X
+DF$X <- row.names(DF)
+
+# Melt the dataset
+library(reshape2)
+
+# Perform chi.sq test (RBMI and SRAGE_P)
+results <- chisq.test(table(adult$RBMI, adult$SRAGE_P))
+
+# Melt results$residuals and store as resid
+resid <- melt(results$residuals)
+
+# Change names of resid
+names(resid) <- c("FILL", "X", "residual")
+
+# merge the two datasets:
+DF_all <- merge(DF_melted, resid)
+
+# Update plot command
+library(ggthemes)
+ggplot(DF_all, aes(ymin = ymin,
+                   ymax = ymax,
+                   xmin = xmin,
+                   xmax = xmax,
+                   fill = residual)) +
+  geom_rect() +
+  scale_fill_gradient2() +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0)) +
+  theme_tufte()
+
+# Plot so far
+p
+
+# Position for labels on y axis (don't change)
+index <- DF_all$xmax == max(DF_all$xmax)
+DF_all$yposn <- DF_all$ymin[index] + (DF_all$ymax[index] - DF_all$ymin[index])/2
+
+# Plot 1: geom_text for BMI (i.e. the fill axis)
+p1 <- p %+% DF_all + 
+  geom_text(aes(x = max(xmax), 
+                y = yposn,
+                label = FILL),
+            size = 3, hjust = 1,
+            show.legend  = FALSE)
+p1
+
+# Plot 2: Position for labels on x axis
+DF_all$xposn <- DF_all$xmin + (DF_all$xmax - DF_all$xmin)/2
+
+# geom_text for ages (i.e. the x axis)
+p1 %+% DF_all + 
+  geom_text(aes(x = xposn, label = X),
+            y = 1, angle = 90,
+            size = 3, hjust = 1,
+            show.legend = FALSE)
+
+# Load all packages
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(ggthemes)
+
+# Script generalized into a function
+mosaicGG
+
+# BMI described by age
+mosaicGG(adult, X = "SRAGE_P", FILL = "RBMI")
+
+# Poverty described by age
+mosaicGG(adult, X = "SRAGE_P", FILL = "POVLL")
+
+# mtcars: am described by cyl
+mosaicGG(mtcars, X = "cyl", FILL = "am")
+
+# Vocab: vocabulary described by education
+library(carData)
+mosaicGG(Vocab, X = "education", FILL = "vocabulary")
