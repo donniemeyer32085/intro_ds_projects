68b9fe4d20144d57d522754ed8f504458ad1a9dd
donniemeyer32085 <123XYZabc#>
2018-08-17 23:04:57 -0700

    machine learning project one

---
+++ machine_learning/linear_regression_script.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+setwd("/home/donniemeyer32085/git/springboard_projects/machine_learning/")
+
+### Linear Regression Project ###
+
+#import data
+wine <- read_csv("wine.csv")
+wine_test <- read_csv("wine_test.csv")
